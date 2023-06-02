28798ef6098bc26f84c986c859d9187062df2d62
Pedro Lemos <lemozxpedro@gmail.com>
2023-06-02 10:04:22 -0300

    Removed default credentials

--- lib/app/authentication/reactivity/login/login_atoms.dart
+++ lib/app/authentication/reactivity/login/login_atoms.dart
@@ -1,5 +1,5 @@
 import 'package:asp/asp.dart';
 
-final loginEmailText = Atom<String>('username@mail.com');
-final loginPasswordText = Atom<String>('Str0ngP@ssw0rd');
+final loginEmailText = Atom<String>('');
+final loginPasswordText = Atom<String>('');
 final loginPasswordTextVisibility = Atom<bool>(true);
